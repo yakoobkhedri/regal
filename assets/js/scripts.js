9537afa1b5f071c0f8237cbd95883d9993904beb
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-02-01 22:11:45 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -335,3 +335,13 @@ faqsTab.forEach((tab) => {
     })
   })
 })
+
+// datapicker
+$(function () {
+  $('input[name="date"]').daterangepicker({
+    singleDatePicker: true,
+    showDropdowns: true,
+    minYear: 1901,
+    maxYear: parseInt(moment().format('YYYY'), 10)
+  });
+});
